040bcbf7289bfaec123195a49d748a1a893560c8
virrrex <virrrex@github.com>
2020-11-29 13:42:04 +0530

    practice

---
+++ ipccert.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n, m, k;
+    cin>>n>>m>>k;
+    vi sum(n);
+    vi minute(n);
+    f(i,n){
+        int s = 0;
+        f(j, k){
+            int t; cin>>t;
+            s += t;
+        }
+        sum[i] = s;
+        cin>>minute[i];
+    }
+    int ans = 0;
+    f(i,n){
+        if(sum[i] >= m && minute[i] <= 10)
+            ans++;
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
