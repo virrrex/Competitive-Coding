063779ed2e4400979e6fd219f50b84a7e784ce17
virrrex <rex.vishal.3@gmail.com>
2020-12-19 22:15:54 +0530

    Practice A

---
+++ Codeforces/208a-Dubstep.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string s, t; cin>>s;
+    int l = 1;
+    f(i, s.length()){
+        if((s[i]=='W') && (s[i+1]=='U') && (s[i+2]=='B')){
+            if(l||t[t.size()-1] == ' ')
+                i += 2;
+            else{
+                i += 2; l = 0;
+                t += " ";
+            }
+        }
+        else{
+            t += s[i];
+            l = 0;
+        }
+    }
+    cout<<t<<endl;
+    return 0;
+}
