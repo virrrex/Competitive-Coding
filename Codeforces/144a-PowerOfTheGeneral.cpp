63eac28f187fc50776c7c20fc2d0d240f66e8e4e
virrrex <rex.vishal.3@gmail.com>
2020-12-19 11:16:13 +0530

    Practice A

---
+++ Codeforces/144a-PowerOfTheGeneral.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n, b, s, big = INT_MIN, small = INT_MAX; cin>>n;
+    vi h(n);
+    f(i,n){
+        cin>>h[i];
+        if(h[i] > big){
+            big = h[i];
+            b = i;
+        }
+        if(h[i] <= small){
+            small = h[i];
+            s = i;
+        }
+    }
+    int d = (s<b ? -1 : 0);
+    cout<<(b + n-1 - s + d)<<endl;
+    return 0;
+}
