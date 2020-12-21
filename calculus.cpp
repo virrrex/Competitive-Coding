0ef7bd919b0e129f9132ac35b441d26f3f6c85ec
virrrex <rex.vishal.3@gmail.com>
2020-12-21 09:42:22 +0530

    December Challenge 2020

---
+++ calculus.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             998244353
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while(b > 0){
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    rex;
+
+    ll n, ans = 1, inc = 3;
+    cin>>n;
+    Fx(i,2,n+1){
+        ans += binpow(inc, mod-2, mod);
+        ans %= mod;
+        inc += 2;
+    }
+    cout<<(2*ans)%mod<<endl;
+
+    return 0;
+}
