97ebaec29eff1ac8d2b272dc8154f2a909766c12
virrrex <rex.vishal.3@gmail.com>
2020-12-31 13:32:34 +0530

    Practice A

---
+++ Codeforces/462a-ApplemanAndEasyTask.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n; cin>>n;
+    vs cell(n);
+    f(i,n) cin>>cell[i];
+    int flag = 0;
+    f(i,n){
+        f(j,n){
+            int s = 0;
+            if(i!=0 && cell[i-1][j]=='o') s++;
+            if(i!=n-1 && cell[i+1][j]=='o') s++;
+            if(j!=0 && cell[i][j-1]=='o') s++;
+            if(j!=n-1 && cell[i][j+1]=='o') s++;
+            if(s%2){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag) break;
+    }
+    if(flag) cout<<"NO\n"; else cout<<"YES\n";
+
+    return 0;
+}
