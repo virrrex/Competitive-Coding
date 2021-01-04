7487a59bf37713b9d02b4a8438261941cb2c4c05
virrrex <rex.vishal.3@gmail.com>
2021-01-04 19:17:44 +0530

    Added makesieve to create array of prime factors

--- primeSieveEratosthenes.cpp
+++ primeSieveEratosthenes.cpp
@@ -10,6 +10,24 @@ bool checkPrime(int n)
     return true;
 }
 
+#define MAXN 1000001 
+long long sieve[MAXN]; 
+void makesieve()
+{
+    sieve[1] = 0; 
+    for (int i = 2; i < MAXN; i++) 
+        sieve[i] = i; 
+    for (int i = 4; i < MAXN; i += 2) 
+        sieve[i] = 2; 
+    for (int i = 3; i * i < MAXN; i++) { 
+        if (sieve[i] == i) { 
+            for (int j = i * i; j < MAXN; j += i) 
+                if (sieve[j] == j) 
+                    sieve[j] = i; 
+        } 
+    } 
+}
+
 void primeSieve(int n)
 {
     int prime[n+1] = {0};
@@ -29,6 +47,8 @@ int main()
     int n = 37;
     cout<<"Is "<<n<<" a Prime number? "<<(checkPrime(n)? "Yes": "No")<<endl;
     cout<<"List of prime numbers till "<<n<<" is: "; primeSieve(n);
+    makesieve();
+    cout<<"\nMake sieve:\n"; for(int i=0; i<n; i++) cout<<sieve[i]<<" ";
 
     return 0;
 }
