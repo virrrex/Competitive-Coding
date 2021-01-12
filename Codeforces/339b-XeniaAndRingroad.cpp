3c2b8950c31caf8c5d12165b5dabe982e3703cff
virrrex <rex.vishal.3@gmail.com>
2021-01-12 12:35:50 +0530

    Practice B

---
+++ Codeforces/339b-XeniaAndRingroad.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n,m; cin>>n>>m;
+    vi tasks(m); f(i,m) cin>>tasks[i];
+    int prev = 1;
+    ll sum = 0;
+    f(i,m){
+        if(tasks[i] < prev)
+            sum += (n-prev + tasks[i]);
+        else 
+            sum += tasks[i]-prev;
+        prev = tasks[i];
+    }
+    cout<<sum<<endl;
+
+    return 0;
+}
