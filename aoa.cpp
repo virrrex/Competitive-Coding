ba45d02fd80ab63cde4739fc5d223bab6b46ae1e
virrrex <rex.vishal.3@gmail.com>
2021-01-12 12:35:24 +0530

    Practice

---
+++ aoa.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        vi num(n); f(i,n)cin>>num[i];
+        int ans = num[0]%mod;
+        fx(i,1,n){
+            ans &= num[i];
+            ans %= mod;
+        }
+        if(__gcd(ans, mod) == 1)
+            cout<<ans<<endl;
+        else 
+            cout<<-1<<endl;
+    }
+
+    return 0;
+}
