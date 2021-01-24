a05f59c0fd2386d353ace1d5d2cfc36a4b69eb8d
virrrex <rex.vishal.3@gmail.com>
2021-01-24 23:17:27 +0530

    Practice B

---
+++ Codeforces/66b-PetyaAndCountryside.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define endl            '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n; cin>>n;
+    vi ar(n); f(i,n) cin>>ar[i];
+    int ans = 1;
+    f(i,n){
+        int temp = 1;
+        for(int j=i-1; j>=0; j--){
+            if(ar[j+1]>=ar[j])
+                temp++;
+            else 
+                break;
+        }
+        for(int j=i+1; j<n; j++){
+            if(ar[j-1]>=ar[j])
+                temp++;
+            else 
+                break;
+        }
+        ans = max(ans, temp);
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
