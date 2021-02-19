d14d975ef543440906c3a0ddcd302354b6c34cbb
virrrex <rex.vishal.3@gmail.com>
2021-02-19 12:36:50 +0530

    Practice A

---
+++ Codeforces/116a-Tram.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n; cin>>n;  
+    int maxCapacity = 0, passengers = 0;
+    f(i,n){
+        int exit, entry;
+        cin>>exit>>entry;
+        passengers = passengers-exit+entry;
+        maxCapacity = max(maxCapacity, passengers);
+    }
+    cout<<maxCapacity<<nl;
+
+    return 0;
+}
