59b3dd771bbe41cb09c7a2bd6214190433b604dc
virrrex <rex.vishal.3@gmail.com>
2021-03-07 10:34:08 +0530

    Practice A

---
+++ Codeforces/41a-Translation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string a, b; cin>>a>>b;
+    if(a.length() != b.length()){cout<<"NO\n"; return 0;}
+    for(int i=0, j=a.length()-1; i<a.length(), j>=0; i++, j--){
+        if(a[i] != b[j]){
+            cout<<"NO\n"; return 0;
+        }
+    }
+    cout<<"YES\n";
+
+    return 0;
+}
