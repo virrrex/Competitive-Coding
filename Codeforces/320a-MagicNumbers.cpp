a662c27c7a7b207eefdafb6eb0ba72e4ab25c1fd
virrrex <rex.vishal.3@gmail.com>
2021-03-07 10:34:30 +0530

    Practice A

---
+++ Codeforces/320a-MagicNumbers.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string s; cin>>s;
+    f(i,s.length()){
+        if(s[i] == '1') continue;
+        if(s[i] == '4' && i>0 && s[i-1] == '1') continue;
+        if(s[i] == '4' && i>1 && s[i-1] == '4' && s[i-2] == '1') continue;
+        cout<<"NO\n";
+        return 0;
+    }
+    cout<<"YES\n";
+    return 0;
+}
