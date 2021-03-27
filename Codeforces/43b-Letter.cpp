3bae3eb331dcb138b091ef2ff2e5203317e96eaa
virrrex <rex.vishal.3@gmail.com>
2021-03-27 10:44:15 +0530

    Practice B

---
+++ Codeforces/43b-Letter.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    map<char,int> mp1, mp2;
+    f(i,s1.length())
+        mp1[s1[i]]++;
+    f(i,s2.length())
+        mp2[s2[i]]++;
+    bool ok = true;
+    for(auto it: mp2)
+        if(it.ff != ' ' && it.ss > mp1[it.ff]){
+            ok = false;
+            break;
+        }
+    if(ok) cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return 0;
+}
