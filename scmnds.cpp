095aad262488c9b08926a547df322bddcbacadea
virrrex <rex.vishal.3@gmail.com>
2021-03-27 10:44:33 +0530

    Practice

---
+++ scmnds.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n,k; cin>>n>>k;
+        string s; cin>>s;
+        vi a(26,0);
+        f(i,n)
+            a[s[i]-'a']++;
+        int ans = 0;
+        for(int m=n; m>0; m--){
+            int g = __gcd(m,k);
+            int ct = m/g;
+            int pos=1, c=0;
+            f(i,26)
+                c += a[i]/ct;
+            if(c >= g){
+                cout<<m<<nl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
