906a8e5ebe0b833ca5a17aa605c803c9927fc847
virrrex <rex.vishal.3@gmail.com>
2021-04-06 10:08:41 +0530

    Practice B

---
+++ Codeforces/160b-UnluckyTicket.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int n; string s; cin>>n>>s;
+    string s1, s2;
+    s1 = s.substr(0,n);
+    s2 = s.substr(n);
+    sortall(s1);
+    sortall(s2);
+    bool less = (s1[0]<s2[0])? true: false;
+    f(i,n){
+        if(less && s1[i]>=s2[i]){
+            cout<<"NO\n";
+            return 0;
+        }
+        if(!less && s1[i]<=s2[i]){
+            cout<<"NO\n";
+            return 0;
+        }
+    }
+    cout<<"YES\n";
+
+    return 0;
+}
