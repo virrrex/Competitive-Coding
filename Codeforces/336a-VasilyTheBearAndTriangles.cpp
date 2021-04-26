648fc7786d98322516fe53f22aa588175f99675c
virrrex <rex.vishal.3@gmail.com>
2021-04-26 11:17:22 +0530

    Practice <1400

---
+++ Codeforces/336a-VasilyTheBearAndTriangles.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int x, y; cin>>x>>y;
+    int sx = (x>0)?1:-1;
+    int sy = (y>0)?1:-1;
+    int val = abs(x)+ abs(y);
+    int x1 = val*sx;
+    int y1 = val*sy;
+    if(x1 < 0)
+        cout<<x1<<" 0 0 "<<y1<<"\n";
+    else    
+        cout<<"0 "<<y1<<" "<<x1<<" 0\n";
+
+    return 0;
+}
