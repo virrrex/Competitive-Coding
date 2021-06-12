865188d425a753d236f72d02b0dfad4610ea1fa0
virrrex <rex.vishal.3@gmail.com>
2021-06-12 22:49:11 +0530

    Codeforces Round #725 (Div. 3)

---
+++ Codeforces/1538a-StoneGame.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        vi a(n); f(i,n) cin>>a[i];
+        int mx = *max_element(all(a));
+        int mn = *min_element(all(a));
+        int steps = 0, ct = 0;
+        f(i,n){
+            steps++;
+            if(a[i] == mn)
+                ct++;
+            if(a[i] == mx)
+                ct++;
+            if(ct == 2)
+            break;
+        }
+        int ans = steps;
+        steps = 0; ct = 0;
+        for(int i=n-1; i>=0; i--){
+            steps++;
+            if(a[i] == mn)
+                ct++;
+            if(a[i] == mx)
+                ct++;
+            if(ct == 2)
+            break;
+        }
+        ans = min(ans, steps);
+        steps = 0;
+        f(i,n){
+            steps++;
+            if(a[i] == mn || a[i] == mx)
+                break;
+        }
+        for(int i = n-1; i>=0; i--){
+            steps++;
+            if(a[i] == mn || a[i] == mx)
+                break;
+        }
+        ans = min(ans, steps);
+        cout<<ans<<nl;
+        
+    }
+
+    return 0;
+}
