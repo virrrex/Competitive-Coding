6b948163c600d74b3de16f5dbbf78507028b282c
virrrex <rex.vishal.3@gmail.com>
2021-07-24 11:36:24 +0530

    Codeforces Round #734 (Div. 3)

---
+++ Codeforces/1551b2-WonderfulColoring2.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+bool fun(pii a, pii b){
+    return a.ss > b.ss;
+}
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n, k; cin>>n>>k;
+        vi a(n); f(i,n) cin>>a[i];
+        unordered_map<int, vi> mp;
+        vi ans(n);
+        int sz = 0;
+        f(i,n)
+            mp[a[i]].pb(i);
+        int idx = 1, store;
+        for(auto it: mp){
+            int s2 = it.ss.size();
+            sz += min(k,s2);
+            if(s2 >= k){
+                f(i,k){
+                    ans[it.ss[i]] = idx;
+                    idx++;
+                    if(idx == k+1)
+                        idx = 1;
+                }
+                f(i,s2 - k)
+                    ans[it.ss[i+k]] = 0;
+            }
+            else{
+                f(i,s2){
+                    ans[it.ss[i]] = idx;
+                    idx++;
+                    if(idx == k+1)
+                        idx = 1;
+                }
+            }
+        }
+        sz /= k;
+        unordered_map<int, int> check;
+        f(i,n){
+            check[ans[i]]++;
+            if(check[ans[i]] <= sz)
+                cout<<ans[i]<<" ";
+            else 
+                cout<<"0 ";
+        }
+        cout<<nl;
+    }
+
+    return 0;
+}
