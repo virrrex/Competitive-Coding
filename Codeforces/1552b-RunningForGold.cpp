7df45bdb0016d7a51d5e74545c6c9ecfbdca4885
virrrex <rex.vishal.3@gmail.com>
2021-07-26 13:14:00 +0530

    Codeforces Global Round 15

---
+++ Codeforces/1552b-RunningForGold.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        vector<vi> athlete(n, vi(5));
+        f(i,n) f(j,5) cin>>athlete[i][j];
+        int gold = 0;
+        fx(i,1,n){
+            int cnt = 0;
+            f(j,5)
+                cnt += athlete[i][j] < athlete[gold][j];
+            if(cnt >= 3)
+                gold = i;
+        }
+        bool wrogn = false;
+        f(i,n){
+            if(i == gold)
+                continue;
+            int cnt = 0;
+            f(j,5)
+                cnt += athlete[i][j] < athlete[gold][j];
+            if(cnt >= 3){
+                wrogn = true;
+                break;
+            }
+        }
+        if(wrogn)
+            cout<<"-1\n";
+        else 
+            cout<<gold+1<<nl;
+    }
+
+    return 0;
+}
