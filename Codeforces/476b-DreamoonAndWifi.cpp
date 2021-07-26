d8ba9947a8c0d96e3e9047bc4620ea21d2f677d1
virrrex <rex.vishal.3@gmail.com>
2021-07-26 22:45:13 +0530

    Practice <1400

---
+++ Codeforces/476b-DreamoonAndWifi.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string in, op; cin>>in>>op;
+    int p1, p2, q;
+    p1 = p2 = q = 0;
+    f(i,in.length()){
+        p1 += (in[i] == '+');
+        p2 += (op[i] == '+');
+        q += (op[i] == '?');
+    }
+    int ans = 0;
+    f(i, 1<<q)
+        if(__builtin_popcount(i) + p2 == p1)
+            ans++;
+
+    cout<<fixed<<setprecision(12)<<(float)ans/(1<<q)<<nl;
+
+    return 0;
+}
