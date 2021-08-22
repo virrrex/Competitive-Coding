08139e4a95476071f196964ee4172da2e4413caa
virrrex <rex.vishal.3@gmail.com>
2021-08-22 21:43:28 +0530

    August 21 long challenge

---
+++ probdiff.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int x, mx = 0;
+        unordered_map<int, int> b; f(i,4) cin>>x, b[x]++;
+        for(auto i: b) mx = max(mx, i.ss);
+        if(b.size() == 1)
+            cout<<"0\n";
+        else if(b.size() == 2){
+            if(mx < 3)
+                cout<<"2\n";
+            else
+                cout<<"1\n";
+        }
+        else
+            cout<<"2\n";
+    }
+
+    return 0;
+}
