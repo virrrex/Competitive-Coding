98196fba30a3f1b6f2a16592f951c3d6d3b8193f
virrrex <rex.vishal.3@gmail.com>
2021-08-27 22:02:29 +0530

    Codeforces Round #739 (Div. 3)

---
+++ Codeforces/1560e-PolycarpAndStringTransformation.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        string s; cin>>s;
+        unordered_map<char,int> mp;
+        string distinct = "";
+        for(int i = s.length()-1; i>=0; i--){
+            mp[s[i]]++;
+            if(mp[s[i]] == 1)
+                distinct = s[i] + distinct;
+        }
+        int anslen = 0, wrong = 0;
+        f(i,distinct.length()){
+            if(mp[distinct[i]] % (i+1) != 0){
+                wrong = 1;
+                break;
+            }
+            anslen += (mp[distinct[i]] / (i+1));
+        }
+        if(wrong){
+            cout<<"-1\n";
+            continue;
+        }
+        string ans = s.substr(0,anslen);
+        string op = ans;
+        string copy = ans;
+        f(i, distinct.length()){
+            copy.erase(remove(all(copy), distinct[i]), copy.end());
+            op += copy;
+        }
+        if(op == s)
+            cout<<ans<<" "<<distinct<<nl;
+        else 
+            cout<<"-1\n";
+    }
+
+    return 0;
+}
