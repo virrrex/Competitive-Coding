ed9dea61663989d9b9e01582e2600b14384bcfa5
virrrex <rex.vishal.3@gmail.com>
2022-02-14 19:32:23 +0530

    Codeforces Global Round 19

---
+++ Codeforces/1637c-AndrewAndStones.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        int mx = -1;
+        vi a(n); f(i,n) {cin>>a[i]; if(i != 0 && i != n-1) mx = max(mx, a[i]);}
+        if(n == 3){
+            if(a[1] & 1)
+                cout<<"-1\n";
+            else 
+                cout<<a[1] / 2<<nl;
+        }
+        else if(mx == 1)
+            cout<<"-1\n";
+        else {
+            ll sm = 0;
+            fx(i,1,n-1)
+                sm += ((ll)a[i] + 1)/2;
+            cout<<sm<<nl;
+        }   // 1 3 4 5
+    }
+
+    return 0;
+}
