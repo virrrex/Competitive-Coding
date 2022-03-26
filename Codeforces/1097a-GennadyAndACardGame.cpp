be84442df813cd5dba1b5f27be064eaeb5f6f1dc
virrrex <rex.vishal.3@gmail.com>
2022-03-26 22:12:27 +0530

    cp sheet practice implementation

---
+++ Codeforces/1097a-GennadyAndACardGame.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    string s; cin>>s;
+    bool flag = false;
+    f(i,5){
+        string x; cin>>x;
+        if(s[0] == x[0]) flag = true;
+        if(s[1] == x[1]) flag = true;
+    }
+    if(flag) cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return 0;
+}
---
+++ Codeforces/1391b-FixYou.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          (x).begin(), (x).end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n,m; cin>>n>>m;
+        vs belt(n);
+        f(i,n) cin>>belt[i];
+        vector<vi> vis(n, vi(m, 0));
+        int ans = 0;
+        f(i,n){
+            f(j, m){
+                if(!vis[i][j]){
+                    int x = i, y = j;
+                    while(belt[x][y] != 'C'){
+                        if(vis[x][y]) break;
+                        vis[x][y] = 1;
+                        if(belt[x][y] == 'R')
+                            y++;
+                        else if(belt[x][y] == 'D')
+                            x++;
+                        
+                        if(x == n){
+                            x--; belt[x][y] = 'R'; ans++;
+                        }
+                        if(y == m){
+                            y--; belt[x][y] = 'D'; ans++;
+                        }
+                    }
+                }
+            }
+        }
+        cout<<ans<<nl;
+    }
+
+    return 0;
+}
