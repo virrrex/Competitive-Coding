31ef3c219dc4f2d3f18811f3a08cf6a952b89b55
Vishal Rex <rex.vishal.3@gmail.com>
2025-12-08 14:48:04 +0530

    practice problems 900 rating from CP-31 tle eliminators

---
+++ Codeforces/1593b-MakeitDivisibleby25.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define ll              long long
+#define f(i,n)          for(int i = 0; i < n; i++)
+#define F(i,n)          for(ll i = 0; i < n; i++)
+#define fx(i,x,n)       for(int i = x; i < n; i++)
+#define Fx(i,x,n)       for(ll i = x; i < n; i++)
+#define nl              '\n'
+#define mod             1000000007
+#define rex             ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(x)          x.begin(), x.end()
+#define sortall(x)      sort(all(x))
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define deb(x)          cout<<#x<<"="<<x<<endl
+#define deb2(x, y)      cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vll;
+typedef vector<string>  vs;
+typedef vector<pii>     vpii;
+typedef vector<pll>     vpll;
+const double PI = 3.141592653589793238460;
+
+ll minimize_operation(string n, string target) {
+    ll n_len = n.length();
+    ll t_len = target.length();
+    ll t_idx = t_len - 1;
+    ll operations = 0;
+
+    for (ll i = n_len - 1; i >= 0 && t_idx >= 0; i--) {
+        if (n[i] == target[t_idx]) {
+            t_idx--;
+        } else {
+            operations++;
+        }
+    }
+
+    if (t_idx >= 0) return LLONG_MAX;
+    return operations;
+}
+
+int main()
+{
+    rex;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        string n; cin>>n;
+        vs candidate25 = {"00", "25", "50", "75"};
+        ll ans = LLONG_MAX;
+        for (auto &x : candidate25) {
+            ans = min(minimize_operation(n, x), ans);
+        }
+        cout<<ans<<nl;
+    }
+    return 0;
+}
